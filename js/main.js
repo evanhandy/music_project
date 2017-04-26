3fb14fc2f12f1ff556af5e9ea11c24a72e78d335
Jon Handy <ninepiratelords@gmail.com>
2017-04-25 20:31:09 -0600

    First Prototype
    Woohoo!

---
+++ js/main.js
@@ -0,0 +1,26 @@
+var piano = new Wad({
+    source : 'square', 
+    env : {
+        attack : .01, 
+        decay : .005, 
+        sustain : .2, 
+        hold : .015, 
+        release : .3
+    }, 
+    filter : {
+        type : 'lowpass', 
+        frequency : 1200, 
+        q : 8.5, 
+        env : {
+            attack : .2, 
+            frequency : 600
+        }
+    }
+})
+
+
+
+$( "button" ).on( "click", function() {
+var noteId = $(this).text();
+piano.play({ pitch : noteId + "4" })
+});
